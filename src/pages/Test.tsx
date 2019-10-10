608a9fdb5f0f17ad1f0eb74edfe52cceabbadf34
reidlee2019 <55223429+reidlee2019@users.noreply.github.com>
2019-10-10 21:13:37 +0800

    Update Test.tsx

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,34 +1,59 @@
 import React from 'react';
-import { Button } from 'antd';
 import connectFn from '@/utils/connectFn';
-import { ConnectProps } from '@/models/connect';
-import WithElementAuth from '@/components/WithElementAuth';
+import Mixins from '@/components/Common/mixin';
 import CreateCard from '@/components/CreateCard';
+import CreateForm from '@/components/CreateForm';
 
-const WithBtn = WithElementAuth(Button);
-
-@connectFn(['login'], ['fakeAccountLogin'])
-class Test extends React.PureComponent<ConnectProps> {
-  handleClick = () => {
-    const { dispatch } = this.props;
-    if (dispatch) {
-      dispatch({
-        type: 'global/changeIsLoading',
-        payload: true,
-      })
+@connectFn([], ['getList'])
+class Test extends Mixins {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      formConfig: {
+        config: [
+          {
+            id: 'userName',
+            type: 'input',
+            options: {
+              name: '账号',
+              value: '',
+              placeholder: '请输入账号',
+            },
+          },
+          {
+            id: 'submit',
+            type: 'button',
+            options: {
+              name: '查询',
+              type: 'primary',
+            },
+          },
+          {
+            id: 'reset',
+            type: 'button',
+            options: {
+              name: '重置',
+            },
+          },
+        ],
+        requestList: this.requestList,
+        changeSearchFormState: this.changeSearchFormState,
+      },
     }
+  }
+
+  public requestList = () => {
+    console.log(111, this.state);
   };
 
   render() {
+    const { formConfig } = this.state;
+    formConfig.changeSearchFormState = this.changeSearchFormState;
     return (
-      <CreateCard
-        addAuth
-        {...this.props}
-      >
-        <p onClick={this.handleClick}>12312312</p>
-        {/* <WithBtn auth="true">1313</WithBtn> */}
+      <CreateCard>
+        <CreateForm formConfig={ formConfig }/>
       </CreateCard>
-    )
+    );
   }
 }
 
